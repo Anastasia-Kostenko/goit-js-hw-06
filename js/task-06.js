6af8734c441a692da1ae44fb4b9a1f6f156fc8e5
Anastasia-Kostenko <nanochka03@gmail.com>
2023-11-06 22:49:50 +0200

    done_task_5_6_7

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const input = document.querySelector("#validation-input");
+
+input.addEventListener("blur", () => {
+    const inputLength = input.value.length;
+    const dataValue = parseInt(input.getAttribute("data-length"));
+    if (inputLength === dataValue) {
+        input.classList.add("valid");
+        input.classList.remove("invalid");
+
+    } else {
+        input.classList.add("invalid");
+        input.classList.remove("valid")
+        }
+ 
+});
