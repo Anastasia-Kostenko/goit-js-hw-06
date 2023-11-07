c66e2f98f94d4fa426185071f56f600696cd6a30
Anastasia-Kostenko <nanochka03@gmail.com>
2023-11-07 22:13:06 +0200

    done_task_08_09

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
-
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    
+    const email = this.elements.email.value;
+    const password = this.elements.password.value;
+    if (email === "example@example.com" && password === "password") {
+        alert("Success");
+    } else {
+        alert("Error");
+    }
+});
